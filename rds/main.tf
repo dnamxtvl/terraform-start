55548ff0b24550a288742c62081efc488efd389b
nam.dovan1 <nam.dovan1@vti.com.vn>
2025-12-29 13:53:12 +0700

    fix: change config rds

--- rds/main.tf
+++ rds/main.tf
@@ -1,22 +1,18 @@
 resource "aws_db_instance" "database" {
-  allocated_storage                     = 20
-  max_allocated_storage                 = 100
-  engine                                = "mysql"
-  engine_version                        = "8.0"
-  instance_class                        = "db.t3.micro"
-  identifier                            = "terraform-db-instance"
-  db_name                               = "quiz_producttion"
-  username                              = var.db_username
-  password                              = var.db_password
-  db_subnet_group_name                  = var.vpc.rds_subnet_group_name
-  vpc_security_group_ids                = [var.sg.db]
-  skip_final_snapshot                   = true
-  multi_az                              = true
-  storage_encrypted                     = true
-  performance_insights_enabled          = true
-  performance_insights_retention_period = 7
-
-  blue_green_update {
-    enabled = true
-  }
+  allocated_storage            = 20
+  max_allocated_storage        = 100
+  engine                       = "mysql"
+  engine_version               = "8.0"
+  instance_class               = "db.t3.micro"
+  identifier                   = "terraform-db-instance"
+  db_name                      = "quiz_producttion"
+  username                     = var.db_username
+  password                     = var.db_password
+  db_subnet_group_name         = var.vpc.rds_subnet_group_name
+  vpc_security_group_ids       = [var.sg.db]
+  skip_final_snapshot          = true
+  multi_az                     = true
+  storage_encrypted            = true
+  performance_insights_enabled = false // true fo config db.t3.medium
+  //performance_insights_retention_period = 7
 }
